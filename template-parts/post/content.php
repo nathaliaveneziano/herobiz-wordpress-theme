ef14a5fb23f6b6bf01a7db8ebb7030dc85661414
nathaliaveneziano <nathaliaveneziano@icloud.com>
2024-04-16 22:30:46 -0300

    implementing single.php

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -36,7 +36,7 @@
       the_content();
 
       wp_link_pages(array(
-        'before'  => '<div class="page-links">' . esc_html__('Pages:', 'custom-classic-theme'),
+        'before'  => '<div class="page-links">' . esc_html__('Pages:', 'herobiz'),
         'after'   => '</div>',
       ));
       ?>
