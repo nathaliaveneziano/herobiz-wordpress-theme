331abff75fdffc940da9536e77c7f21c2f7c070d
nathaliaveneziano <nathaliaveneziano@icloud.com>
2024-04-16 22:16:46 -0300

    implementing single.php

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -10,7 +10,11 @@
   <!-- Post title -->
   <header class="entry-header">
     <?php
+    if (is_singular()) :
+      the_title('<h1 class="entry-title">', '</h1>');
+    else :
       the_title('<h2 class="entry-title"><a class="entry-link" href="' . esc_url(get_permalink()) . '">', '</a></h2>');
+    endif;
     ?>
   </header>
 
@@ -20,10 +24,22 @@
     the_post_thumbnail('large');
   endif;
   ?>
-  
+
   <!-- Post content -->
+  <?php if (is_home() || is_archive()) : ?>
     <div class="entry-content">
       <?php the_excerpt(); ?>
+    </div>
+  <?php elseif (is_single()) : ?>
+    <div class="entry-content">
+      <?php
+      the_content();
+
+      wp_link_pages(array(
+        'before'  => '<div class="page-links">' . esc_html__('Pages:', 'custom-classic-theme'),
+        'after'   => '</div>',
+      ));
+      ?>
     </div>
   <?php endif; ?>
 </article>
